a241fe3099cf2204b239e49485590aa10bd0ec29
Armianto Sumitro <armiantos@gmail.com>
2026-01-30 23:23:48 -0700

    Setup roslyn

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,3 +11,25 @@ vim.keymap.set('n', '<leader>P', builtin.commands)
 
 -- nvim-tree config
 vim.keymap.set('n', '<leader>b', ':NvimTreeToggle<CR>')
+
+require("mason").setup({
+    registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+    },
+})
+
+vim.lsp.config("roslyn", {
+    on_attach = function()
+        print("This will run when the server attaches!")
+    end,
+    settings = {
+        ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+        },
+        ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+        },
+    },
+})
