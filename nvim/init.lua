bdc08674ed9474643b1532f67306d2f64eaef3b5
Armianto Sumitro <armiantos@gmail.com>
2026-02-01 16:40:40 -0700

    Switch to find_files (already ignores gitignore)

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,7 @@ require("config.lazy")
 -- telescope config
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>p', builtin.git_files)
+vim.keymap.set('n', '<leader>p', builtin.find_files)
 vim.keymap.set('n', '<leader>F', builtin.live_grep)
 vim.keymap.set('n', '<leader>P', builtin.commands)
 
