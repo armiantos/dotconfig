3414ef72ab8c4bb0e31ff641f9071335edd5ac3d
Armianto Sumitro <armiantos@gmail.com>
2026-02-15 10:24:57 -0700

    Sync nvim tree on cd

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,8 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      sync_root_with_cwd = true
+    }
   end,
 }
