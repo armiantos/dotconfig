b26a61262bcc21ed59e9d0cc791d1669ad8317db
Armianto Sumitro <armiantos@gmail.com>
2026-02-22 19:50:23 -0700

    Allow toggle format on save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,12 +1,14 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
-    formatters_by_ft = {
-      json = { "jq" },
-    },
-  },
+	'stevearc/conform.nvim',
+	opts = {
+		format_on_save = function(bufnr)
+			if vim.g.disable_autoformat then
+				return
+			end
+			return { timeout_ms = 500, lsp_format = "fallback" }
+		end,
+		formatters_by_ft = {
+			json = { "jq" },
+		},
+	},
 }
