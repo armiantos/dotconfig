64cafb3744cffefe513d30c69297a8788e1732ea
Armianto Sumitro <armiantos@gmail.com>
2026-02-22 19:50:23 -0700

    Allow toggle format on save

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,10 +33,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = "Remove highlights 
 vim.keymap.set('n', '<leader>b', ':NvimTreeToggle<CR>', { desc = "Toggle file tree" })
 
 require("mason").setup({
-    registries = {
-        "github:mason-org/mason-registry",
-        "github:Crashdummyy/mason-registry",
-    },
+	registries = {
+		"github:mason-org/mason-registry",
+		"github:Crashdummyy/mason-registry",
+	},
 })
 
 -- vim.lsp.enable('roslyn') -- already enabled through roslyn.nvim
@@ -44,22 +44,30 @@ vim.lsp.enable('clangd')
 
 -- Diagnostic Config & Keymaps
 vim.diagnostic.config {
-  update_in_insert = false,
-  severity_sort = true,
-  float = { border = 'rounded', source = 'if_many' },
-  underline = { severity = vim.diagnostic.severity.ERROR },
+	update_in_insert = false,
+	severity_sort = true,
+	float = { border = 'rounded', source = 'if_many' },
+	underline = { severity = vim.diagnostic.severity.ERROR },
 
-  -- Can switch between these as you prefer
-  virtual_text = true, -- Text shows up at the end of the line
-  virtual_lines = false, -- Teest shows up underneath the line, with virtual lines
+	-- Can switch between these as you prefer
+	virtual_text = true, -- Text shows up at the end of the line
+	virtual_lines = false, -- Teest shows up underneath the line, with virtual lines
 
-  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
-  jump = { float = true },
+	-- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+	jump = { float = true },
 }
 
 -- nvim-treesitter
 require'nvim-treesitter'.install { 'c_sharp' }
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'cs' },
-  callback = function() vim.treesitter.start() end,
+	pattern = { 'cs' },
+	callback = function() vim.treesitter.start() end,
+})
+
+-- conform
+vim.api.nvim_create_user_command("ToggleFormatOnSave", function(args)
+	vim.g.disable_autoformat = not vim.g.disable_autoformat
+end, {
+desc = "Toggle autoformat-on-save",
+bang = true,
 })
