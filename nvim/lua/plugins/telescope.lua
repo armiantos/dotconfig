96b9f4aa52ef26735223069ae834a12a63256d17
Armianto Sumitro <armiantos@gmail.com>
2026-03-03 20:05:21 -0700

    Grep fixed strings only

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,30 +1,40 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    },
-    extensions = {
-      ['ui-select'] = {
-          require('telescope.themes').get_dropdown()
-      }
-    },
-    config = function()
-        require('telescope').load_extension('ui-select')
-        require('telescope').load_extension('fzf')
-        require('telescope').setup{
-          pickers = {
-            colorscheme = {
-              enable_preview = true,
-            },
-          },
-          defaults = {
-              layout_strategy = 'flex',
-              layout_config = {
-                  flip_columns = 160
-              }
-          }
-        }
-    end
+	'nvim-telescope/telescope.nvim',
+	version = '*',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		-- optional but recommended
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+	},
+	extensions = {
+		['ui-select'] = {
+			require('telescope.themes').get_dropdown()
+		}
+	},
+	config = function()
+		require('telescope').load_extension('ui-select')
+		require('telescope').load_extension('fzf')
+		require('telescope').setup {
+			pickers = {
+				colorscheme = {
+					enable_preview = true,
+				},
+			},
+			defaults = {
+				layout_strategy = 'flex',
+				layout_config = {
+					flip_columns = 160
+				},
+				vimgrep_arguments = {
+					"rg",
+					"-F", -- Fixed strings only
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case"
+				} }
+		}
+	end
 }
