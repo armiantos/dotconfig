44e9d87b689a04af2ed6471db75761a8f51d4e7f
Armianto Sumitro <armiantos@gmail.com>
2026-05-16 20:17:52 -0600

    Disable git config in nvimtree to ease CPU

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,10 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
+	'nvim-treesitter/nvim-treesitter',
+	lazy = false,
+	build = ':TSUpdate',
+	opts = {
+		git = {
+			enable = false
+		}
+	}
 }
