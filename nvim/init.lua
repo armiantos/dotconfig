5be6c7f79b94fba3591ee7c2df9c15dac723780a
Armianto Sumitro <armiantos@gmail.com>
2026-05-25 09:37:18 -0600

    Remap leader to space

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,14 @@
-vim.g.mapleader = "`"
+vim.g.mapleader = " "
 vim.opt.number = true
 
 require("config.lazy")
 
 -- telescope config
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>p', builtin.git_files)
+vim.keymap.set('n', '<leader>b', builtin.buffers)
+vim.keymap.set('n', '<leader>h', builtin.help_tags)
 
 -- vim plug
 local vim = vim
