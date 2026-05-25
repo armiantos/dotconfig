4ec2b19bba2a2edd71b2e56d04ddfbbf52c72752
Armianto Sumitro <armiantos@gmail.com>
2026-05-25 09:38:05 -0600

    Display references through telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,6 +19,9 @@ vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>F', builtin.live_grep, { desc = "Global search" })
 vim.keymap.set('n', '<leader>P', builtin.commands, { desc = "Commands" })
 
+vim.keymap.set('n', 'grr', builtin.lsp_references, { desc = "List references" })
+vim.keymap.set('n', 'gri', builtin.lsp_implementations, { desc = "List implementations" })
+
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
 
