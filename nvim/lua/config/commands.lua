4d12b25292bee9308f7cba817a0c753fcfdda49e
Armianto Sumitro <armiantos@gmail.com>
2026-07-20 22:10:14 -0600

    Add GSetBase and GdiffBase to ease adhoc diff viewing

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -9,3 +9,22 @@ vim.api.nvim_create_user_command("Format", function(args)
 	end
 	require("conform").format({ async = true, lsp_format = "fallback", range = range })
 end, { range = true })
+
+vim.api.nvim_create_user_command("GSetBase", function(opts)
+	local base_commit = opts.fargs[1]
+	vim.g.fugitive = {
+		base = base_commit
+	}
+	print(string.format('Set base commit to %s', base_commit))
+	vim.cmd(string.format('G difftool %s --name-status', base_commit))
+	vim.cmd('GdiffBase')
+end, { nargs = 1})
+
+vim.api.nvim_create_user_command("GdiffBase", function()
+	if not vim.g.fugitive then
+		print('No base commit set')
+	end
+
+	print(string.format('Diffing against %s', vim.g.fugitive.base))
+	vim.cmd(string.format('Gdiffsplit %s', vim.g.fugitive.base))
+end, {})
--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,8 +1,20 @@
+local close_diff_and_reinit_diff = function(mode)
+	if vim.wo.diff then
+		vim.cmd.normal({'dq'}) -- close diff
+	end
+
+	vim.cmd(mode) -- go to next fileS
+	vim.cmd(':GdiffBase')
+end
+
 return {
 	'tpope/vim-fugitive',
 	lazy = false,
 	keys = {
 		{ '<leader>ls', '<cmd>G<CR>', desc = "Open git" },
 		{ '<leader>ly', ':GBrowse!<CR>',  mode = { 'n', 'v' }, desc = "Copy permalink" },
+		{ '<leader>ld', ':GdiffBase<CR>', desc = 'Start diffing from base set by `<leader>gb`' },
+		{ '<leader>ln', function() close_diff_and_reinit_diff('cnext') end, desc = 'Start diffing from base set by `<leader>gb`' },
+		{ '<leader>lp', function() close_diff_and_reinit_diff('cprev') end, desc = 'Start diffing from base set by `<leader>gb`' },
 	},
 }
