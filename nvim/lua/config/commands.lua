a51580fb7beebdb8c5edfede054ba1644929a755
Armianto Sumitro <armiantos@gmail.com>
2026-07-22 09:54:02 -0700

    Add inspect command to load commit to quickfix

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -10,15 +10,32 @@ vim.api.nvim_create_user_command("Format", function(args)
 	require("conform").format({ async = true, lsp_format = "fallback", range = range })
 end, { range = true })
 
+vim.api.nvim_create_user_command("GInspect", function(opts)
+	local commit = opts.fargs[1]
+	local cmd = string.format('GSetBase %s~1 %s', commit, commit)
+	print(cmd)
+	vim.cmd(cmd)
+end, { nargs = 1, desc = 'Loads commit diff to quickfix' })
+
 vim.api.nvim_create_user_command("GSetBase", function(opts)
 	local base_commit = opts.fargs[1]
 	vim.g.fugitive = {
 		base = base_commit
 	}
+
+	if #opts.fargs > 1 then
+		local head = opts.fargs[2]
+		print(string.format('Set base commit to %s and target %s', base_commit, head))
+		vim.g.fugitive = {
+			base = base_commit,
+			head = head
+		}
+		vim.cmd(string.format('G difftool %s..%s --name-status', base_commit, head))
+		return
+	end
 	print(string.format('Set base commit to %s', base_commit))
 	vim.cmd(string.format('G difftool %s --name-status', base_commit))
-	vim.cmd('GdiffBase')
-end, { nargs = 1})
+end, { nargs = '+', desc = 'Sets commit base and optionally target - defaults to current work dir' })
 
 vim.api.nvim_create_user_command("GdiffBase", function()
 	if not vim.g.fugitive then
@@ -26,5 +43,5 @@ vim.api.nvim_create_user_command("GdiffBase", function()
 	end
 
 	print(string.format('Diffing against %s', vim.g.fugitive.base))
-	vim.cmd(string.format('Gdiffsplit %s', vim.g.fugitive.base))
-end, {})
+	vim.cmd(string.format('Gvdiffsplit %s', vim.g.fugitive.base))
+end, { desc = 'Compares current file against base diff set from GSetBase' })
