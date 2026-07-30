ba4de28beda507dda5264071744be6590bafd079
Armianto Sumitro <armiantos@gmail.com>
2026-07-29 22:36:34 -0600

    Fix telescope install failure

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,24 +8,24 @@ return {
 		'nvim-telescope/telescope-ui-select.nvim',
 	},
 	keys = {
-		{ '<leader>p', function() require('telescope.builtin').find_files() end, desc = "Find files" },
-		{ '<leader><C-p>', function() require('telescope.builtin').find_files({ hidden = true }) end, desc = "Find files (with hidden files)" },
-		{ '<leader>b', function() require('telescope.builtin').buffers() end, desc = "Buffers" },
-		{ '<leader>F', function() require('telescope.builtin').live_grep() end, desc = "Global search" },
-		{ '<leader>P', function() require('telescope.builtin').commands() end, desc = "Commands" },
-		{ '<leader>;', function() require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') }) end, desc = "Find config files" },
-		{ 'grr', function() require('telescope.builtin').lsp_references() end, desc = "List references" },
-		{ 'gri', function() require('telescope.builtin').lsp_implementations() end, desc = "List implementations" },
-	},
-	extensions = {
-		['ui-select'] = {
-			require('telescope.themes').get_dropdown()
-		}
+		{ '<leader>p',     function() require('telescope.builtin').find_files() end,                                   desc = "Find files" },
+		{ '<leader><C-p>', function() require('telescope.builtin').find_files({ hidden = true }) end,                  desc = "Find files (with hidden files)" },
+		{ '<leader>b',     function() require('telescope.builtin').buffers() end,                                      desc = "Buffers" },
+		{ '<leader>F',     function() require('telescope.builtin').live_grep() end,                                    desc = "Global search" },
+		{ '<leader>P',     function() require('telescope.builtin').commands() end,                                     desc = "Commands" },
+		{ '<leader>;',     function() require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') }) end, desc = "Find config files" },
+		{ 'grr',           function() require('telescope.builtin').lsp_references() end,                               desc = "List references" },
+		{ 'gri',           function() require('telescope.builtin').lsp_implementations() end,                          desc = "List implementations" },
 	},
 	config = function()
 		require('telescope').load_extension('ui-select')
 		require('telescope').load_extension('fzf')
 		require('telescope').setup {
+			extensions = {
+				['ui-select'] = {
+					require('telescope.themes').get_dropdown()
+				}
+			},
 			pickers = {
 				colorscheme = {
 					enable_preview = true,
